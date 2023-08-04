016c0b725d67add776d3f3e34ec99b1d56f6d92e
tanish0510 <tanishseth0510@gmail.com>
2023-08-04 20:30:18 +0530

    Day 1 project

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to Band Name Generator....")
+
+bandname_1=input("What is your clan name?\n")
+bandname_2=input("what is your gaming name?\n")
+
+print("Your Band name is ready an being processed....")
+print("\n")
+
+print(bandname_1+bandname_2)
