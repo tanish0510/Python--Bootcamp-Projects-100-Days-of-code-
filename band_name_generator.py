2a4da393979d5a3d5af06decb09762c328d5ed1f
tanish0510 <tanishseth0510@gmail.com>
2023-08-12 01:55:23 +0530

    python

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,5 +1,6 @@
 print("Welcome to Band Name Generator....")
 
+
 bandname_1=input("What is your clan name?\n")
 bandname_2=input("what is your gaming name?\n")
 
---
+++ while_loops.py
@@ -0,0 +1,5 @@
+a=3
+while a>0:
+    print("hello")
+    a=a-1
+    print(f"the number now is {a}")
