d4a6c87d63c1465277e47343e78cbf99cd20d102
tanish0510 <tanishseth0510@gmail.com>
2023-08-20 01:36:51 +0530

    new projects

---
+++ package.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+
+table=PrettyTable()
+
+table.add_column("people",["tanish","inder","seth"])
+table.add_column("people",["tanish","inder","seth"])
+
+print(table)
---
+++ turtle.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+timmy=Turtle()
+
+my_screen=Screen()
+my_screen.exitonclick()
+timmy.color("red")
+timmy.forward(200)
+print(my_sreen.canvheight)
