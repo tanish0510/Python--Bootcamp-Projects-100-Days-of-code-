f55e40e9b7d348d13025db2004a1ec6d0313bcc2
tanish0510 <tanishseth0510@gmail.com>
2024-03-01 23:27:15 +0530

    new more

--- list.py
+++ list.py
@@ -1,23 +1,37 @@
-my_list = []
-for i in range(10):
-    my_list.append(i)
-    # print(my_list)
-
-my_list.insert(2,"hello")
-print(my_list)
-
-# deleting element
-del my_list[7]
-my_list.remove("hello")
-print(my_list)
-
-
-a=my_list.pop(8)
-print(f"popped element is {a}")
-print("returning new list after")
-print(my_list)
-print("count:")
-print(my_list.count(9))
+# numbers=[1,2,3,4,5,6,7,8,9]
+#
+# sq_num=[num*num for num in numbers]
+#
+# print(sq_num)
+#
+#
+#
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+#
+#
+# even_list=[num for num in numbers if num%2 ==0]
+# print(even_list)
+#
+# odd_list=[num for num in numbers if num%2 !=0]
+# print(odd_list)
+#
+#
 
 
-print(my_list.reverse())
+#
+# with open("file1.txt") as file1:
+#     list1 = file1.readlines()
+#
+# with open("file2.txt") as file2:
+#     list2 = file2.readlines()
+#
+#
+# result=[int(num) for num in list1 if num in list2]
+#
+# print(result)
+
+
+weather_c=eval(input("Enter the temperature in Celsius:"))
+
+weather_f={day:temp*9/5 +32 for (day,temp) in weather_c.items()}
+print(weather_f)
